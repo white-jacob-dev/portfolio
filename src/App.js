ef874bb795606698223198050b1d4883a8ef566f
Jacob White <jacob@Jacobs-MBP.hsd1.wv.comcast.net>
2022-03-01 22:52:10 -0500

    about section template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import avatar from './Avatar.svg';
 
 function App() {
   const [showMenu, setShowMenu] = useState(false);
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <div>
-      <nav className="w-full h-14 flex justify-between items-center bg-slate-300">
+      <nav className="w-full h-14 flex justify-between items-center">
         <div className="flex flex-row ">
           <svg className="w-8 h-8 fill-black" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3.88596 0.126797C3.84238 0.196551 4.22188 0.948394 4.7293 1.79756L5.65203 3.34162H9.03114C10.8897 3.34162 12.41 3.38854 12.4097 3.44604C12.4094 3.50354 12.2932 3.73842 12.1516 3.96815C12.0099 4.19788 11.7338 4.66778 11.538 5.01238C10.2981 7.19537 7.40794 12.0743 7.26338 12.2283C7.09556 12.407 6.58265 11.6454 5.07565 8.98043C4.90247 8.67413 4.53285 8.032 4.25418 7.55333L3.74762 6.68314H1.87381C0.829372 6.68314 0 6.7398 0 6.81109C0 6.8814 0.380769 7.5756 0.846154 8.3539C1.31154 9.13219 1.69231 9.79396 1.69231 9.82473C1.69231 9.85536 1.93445 10.2732 2.23046 10.7531C2.52647 11.2332 3.71067 13.2548 4.862 15.2458C6.01333 17.2368 7.00108 18.9177 7.05678 18.9812C7.16368 19.1029 7.41301 18.7128 9.30967 15.4546C9.95613 14.3443 10.9073 12.7153 11.4236 11.8346C12.5083 9.98456 13.9153 7.58075 16.4205 3.29748C17.3976 1.62672 18.1606 0.201285 18.1159 0.129999C18.0087 -0.0412536 3.99314 -0.0443165 3.88596 0.126797ZM16.9936 8.86515C16.2955 10.0631 15.5429 11.3624 15.321 11.7522C15.0992 12.1422 14.4805 13.1933 13.946 14.0881C13.4117 14.9829 12.9744 15.7575 12.9744 15.8094C12.9744 15.9195 14.6409 18.8129 14.7988 18.9769C14.8578 19.038 15.243 18.5054 15.6553 17.7934C16.0675 17.0814 16.8276 15.7783 17.3445 14.8977C17.8613 14.0171 18.8132 12.3881 19.4598 11.2777C20.1065 10.1674 20.9424 8.74903 21.3177 8.1257C21.693 7.50251 22 6.92289 22 6.83782C22 6.73215 21.4088 6.6837 20.1314 6.68509L18.2628 6.6869L16.9936 8.86515Z" />
@@ -43,16 +44,50 @@ function App() {
           <div>contact</div>
         </div>
       ) : null}
-      <div className="text-center">
+      <div className="text-center space-y-4 md:text-left ">
         <div className="text-left">&lt;intro&gt;</div>
-        <div>Hi, my name is Jacob White</div>
+        <div>Hi, my name is Jacob White.</div>
         <div>I design and build for the web.</div>
         <div>
           I'm a ui/ux designer and front-end developer who creates unique & interactive digital experiences with a human-centered approach. Currently,
           I'm focused on designing and building world-class applications with my team at M&S Consulting.
         </div>
-        <button class="py-2 px-4  bg-indigo-600 text-white">Send me an email</button>
+        <button className="py-2 px-4 bg-blue-600 text-white rounded-lg">Send me an email</button>
         <div className="text-left">&lt;/intro&gt;</div>
+      </div>
+      <div className="space-y-4">
+        <div className="text-left">&lt;about&gt;</div>
+        <div className="flex flex-col space-y-4 md:flex-row md:items-center">
+          <img src={avatar} className="max-h-48"></img>
+          <div className="text-center md:text-right">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eleifend nunc, vel amet mauris non. Ut varius ultrices a, lacus in. Aliquam
+            risus, tortor egestas dui ipsum tincidunt placerat.
+          </div>
+        </div>
+        <div className="grid grid-cols-5 grid-rows-4 text-center border-4 rounded-lg">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div>10</div>
+          <div>11</div>
+          <div>12</div>
+          <div>13</div>
+          <div>14</div>
+          <div>15</div>
+          <div>16</div>
+          <div>17</div>
+          <div>18</div>
+          <div>19</div>
+          <div>20</div>
+        </div>
+
+        <div className="text-left">&lt;/about&gt;</div>
       </div>
     </div>
   );
