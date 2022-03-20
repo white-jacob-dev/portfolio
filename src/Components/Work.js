58bab49ae74c2c1192b2627bfd7d5f5955833f43
Jacob White <white.jacob.dev@gmail.com>
2022-03-19 23:47:33 -0400

    added iconic air to work section

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -4,11 +4,20 @@ import gigupdesktop from '../Assets/gigupdesktop.png';
 import gigupmobile from '../Assets/gigupmobile.png';
 import goghdesktop from '../Assets/goghdesktop.png';
 import goghmobile from '../Assets/goghmobile.png';
+import iconicairdesktop from '../Assets/iconicairdesktop.png';
+import iconicairmobile from '../Assets/iconicairmobile.png';
 import react from '../Assets/react.svg';
 import firebase from '../Assets/firebase.svg';
 import mui from '../Assets/mui.svg';
 import expo from '../Assets/expo.svg';
 import nodejs from '../Assets/nodejs.svg';
+import formik from '../Assets/formik.svg';
+import sass from '../Assets/sass.svg';
+import angular from '../Assets/angular.svg';
+import jest from '../Assets/jest.svg';
+import chartjs from '../Assets/chartjs.svg';
+import typescript from '../Assets/typescript.svg';
+import tailwind from '../Assets/tailwind.svg';
 
 export function Work() {
   return (
@@ -60,6 +69,12 @@ export function Work() {
                   </div>
                   <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
                     <div className="w-full h-full flex justify-center items-center">
+                      <img src={formik} className="max-h-6"></img>
+                    </div>
+                    <div className="text-center text-xxs">Formik</div>
+                  </div>
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
+                    <div className="w-full h-full flex justify-center items-center">
                       <img src={expo} className="max-h-5"></img>
                     </div>
                     <div className="text-center text-xxs">Expo</div>
@@ -85,29 +100,86 @@ export function Work() {
                 </div>
 
                 <div className="flex flex-row justify-start space-x-4">
-                  <div className="bg-white rounded-lg shadow-lg w-20 h-20 flex flex-col p-2 justify-evenly  transition-all duration-500 hover:scale-110">
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
                     <div className="w-full h-full flex justify-center items-center">
-                      <img src={react} className="max-h-9"></img>
+                      <img src={react} className="max-h-6"></img>
                     </div>
-                    <div className="text-center text-xs">React</div>
+                    <div className="text-center text-xxs">React</div>
                   </div>
-                  <div className="bg-white rounded-lg shadow-lg w-20 h-20 flex flex-col p-2 justify-evenly  transition-all duration-500 hover:scale-110">
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
                     <div className="w-full h-full flex justify-center items-center">
-                      <img src={nodejs} className="max-h-12"></img>
+                      <img src={nodejs} className="max-h-6"></img>
                     </div>
-                    <div className="text-center text-xs">Node</div>
+                    <div className="text-center text-xxs">Node.js</div>
                   </div>
-                  <div className="bg-white rounded-lg shadow-lg w-20 h-20 flex flex-col p-2 justify-evenly  transition-all duration-500 hover:scale-110">
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
                     <div className="w-full h-full flex justify-center items-center">
-                      <img src={mui} className="max-h-9"></img>
+                      <img src={sass} className="max-h-6"></img>
                     </div>
-                    <div className="text-center text-xs">MUI</div>
+                    <div className="text-center text-xxs">Sass</div>
                   </div>
-                  <div className="bg-white rounded-lg shadow-lg w-20 h-20 flex flex-col p-2 justify-evenly  transition-all duration-500 hover:scale-110">
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
                     <div className="w-full h-full flex justify-center items-center">
-                      <img src={expo} className="max-h-8"></img>
+                      <img src={formik} className="max-h-6"></img>
                     </div>
-                    <div className="text-center text-xs">Expo</div>
+                    <div className="text-center text-xxs">Formik</div>
+                  </div>
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
+                    <div className="w-full h-full flex justify-center items-center">
+                      <img src={expo} className="max-h-5"></img>
+                    </div>
+                    <div className="text-center text-xxs">Expo</div>
+                  </div>
+                </div>
+              </div>
+              <div className="space-y-4 ">
+                <div className="flex flex-col space-y-4 justify-between">
+                  <div className="flex flex-row justify-between">
+                    <div className="text-5xl font-bold text-left">Iconic Air</div>
+                    <div className="text-xs basis-1/5 font-semibold text-right self-end">Web Development</div>
+                  </div>
+                  <div className="basis-3/5 text-left text-white text-xs font-base py-3 px-4 bg-blue-600  rounded-lg shadow-lg">
+                    Contracted with the U.S. Air Force and on the Forbes 30/30 list, Iconic Air pioneers a new way of emissions monitoring and data
+                    reporting for the oil and gas industry. Runs on many custom built sensor components and uses Chart.js to help platform users
+                    visualize their data in the format of their liking.
+                  </div>
+                </div>
+                <div>
+                  <a href="https://iconicair.io/" target="_blank">
+                    <img src={iconicairmobile} className="w-full bg-slate-100 rounded-2xl shadow-lg"></img>
+                  </a>
+                </div>
+
+                <div className="flex flex-row justify-start space-x-4">
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
+                    <div className="w-full h-full flex justify-center items-center">
+                      <img src={angular} className="max-h-6"></img>
+                    </div>
+                    <div className="text-center text-xxs">Angular</div>
+                  </div>
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
+                    <div className="w-full h-full flex justify-center items-center">
+                      <img src={typescript} className="max-h-5"></img>
+                    </div>
+                    <div className="text-center text-xxs">Typescript</div>
+                  </div>
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
+                    <div className="w-full h-full flex justify-center items-center">
+                      <img src={tailwind} className="max-h-6"></img>
+                    </div>
+                    <div className="text-center text-xxs">Tailwind</div>
+                  </div>
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
+                    <div className="w-full h-full flex justify-center items-center">
+                      <img src={firebase} className="max-h-7"></img>
+                    </div>
+                    <div className="text-center text-xxs">Firebase</div>
+                  </div>
+                  <div className="bg-white rounded-lg shadow-lg w-12 h-12 flex flex-col p-1 justify-evenly  transition-all duration-500 hover:scale-110">
+                    <div className="w-full h-full flex justify-center items-center">
+                      <img src={chartjs} className="max-h-6"></img>
+                    </div>
+                    <div className="text-center text-xxs">Chart.js</div>
                   </div>
                 </div>
               </div>
